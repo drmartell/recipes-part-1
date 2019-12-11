882a5309643f8b0edd63fab736f4ba40dc27a75d
Casey Martell <drmartell@gmail.com>
2019-12-11 15:27:27 -0800

    tests passing for attempts

---
+++ __tests__/attempts.test.js
@@ -0,0 +1,121 @@
+require('dotenv').config();
+
+const request = require('supertest');
+const app = require('../lib/app');
+const connect = require('../lib/utils/connect');
+const mongoose = require('mongoose');
+const Attempt = require('../lib/models/Attempt');
+const Recipe = require('../lib/models/Recipe');
+
+describe('attempt routes', () => {
+  beforeAll(() => connect());
+
+  beforeEach(() => mongoose.connection.dropDatabase());
+
+  let recipe;
+  let attempt;
+  beforeEach(async() => {
+    recipe = await Recipe.create({ name: 'bread', ingredients: [], directions: [] });
+    attempt = { recipeId: recipe._id, dateOfAttempt: Date.now(), notes: 'killer bread', rating: 5 };
+  });
+
+  afterAll(() => mongoose.connection.close());
+
+  it('creates an attempt', () => request(app)
+    .post('/api/v1/attempts')
+    .send(attempt)
+    .then(res => {
+      expect(res.body).toEqual({
+        _id: expect.any(String),
+        recipeId: recipe._id.toString(),
+        dateOfAttempt: expect.any(String),
+        notes: 'killer bread',
+        rating: 5,
+        __v: 0
+      });
+    }));
+
+  it('gets all attempts', async() => {
+    const attempts = await Attempt.create([
+      attempt,
+      { ...attempt, rating: 4 },
+      { ...attempt, rating: 3 },
+      { ...attempt, rating: 2 },
+    ]);
+
+    return request(app)
+      .get('/api/v1/attempts')
+      .then(res => {
+        attempts.forEach(attempt => {
+          expect(res.body).toContainEqual(JSON.parse(JSON.stringify(attempt)));
+        });
+      });
+  });
+
+  it('gets an attempt by id', async() => {
+    const attempt = await Attempt.create({
+      recipeId: recipe._id,
+      dateOfAttempt: Date.now(),
+      notes: 'It went well',
+      rating: 4
+    });
+
+    return request(app)
+      .get(`/api/v1/attempts/${attempt._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          recipeId: recipe._id.toString(),
+          dateOfAttempt: expect.any(String),
+          notes: 'It went well',
+          rating: 4,
+          __v: 0
+        });
+      });
+  });
+
+  it('updates an attempt by id', async() => {
+    const attempt = await Attempt.create({
+      recipeId: recipe._id,
+      dateOfAttempt: Date.now(),
+      notes: 'It went well',
+      rating: 4
+    });
+
+    return request(app)
+      .patch(`/api/v1/attempts/${attempt._id}`)
+      .send({ rating: 5 })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          recipeId: recipe._id.toString(),
+          dateOfAttempt: expect.any(String),
+          notes: 'It went well',
+          rating: 5,
+          __v: 0
+        });
+      });
+  });
+
+  it('deletes an attempt by id', async() => {
+    const attempt = await Attempt.create({
+      recipeId: recipe._id,
+      dateOfAttempt: Date.now(),
+      notes: 'It went well',
+      rating: 4
+    });
+
+    return request(app)
+      .delete(`/api/v1/attempts/${attempt._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          recipeId: recipe._id.toString(),
+          dateOfAttempt: expect.any(String),
+          notes: 'It went well',
+          rating: 4,
+          __v: 0
+        });
+      });
+  });
+});
